aa913ffc186aa6cb51d0a1f214c32f64ea4e114d
ShahidKhan48 <awskhan777@gmail.com>
2024-10-18 14:27:10 +0530

    this is first java programing on intellijeIdea

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("the sum is :");
+        int num1 = 5;
+        int num2 = 6;
+        int sum = num1 + num2;
+        System.out.printf(String.valueOf(sum));
+    }
+
+
+}
